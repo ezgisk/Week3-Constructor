ca2c7d9a5c2e4283104b5f64553fc7e1847f88c5
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-14 20:44:41 -0700

    Add project files.

---
+++ Week3-Constructor/Constructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Constructor
+{
+    public class Bebek
+    {
+        // Fields
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Bebek() //This constructor doesn't take any parameters
+        {
+            Birthday = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+        public Bebek(string name, string surname) //This constructor takes name and surname as parameters
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Birthday = DateTime.Now;   
+
+            Console.WriteLine("Ingaaaa");
+
+        }
+    }
+}
---
+++ Week3-Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Week3_Constructor;
+
+Bebek bebek =  new Bebek();//Creating a baby using default constructor
+
+bebek.Name = "Defne";
+bebek.Surname = "Gecimli";
+
+Console.WriteLine($"Bebegin adi : {bebek.Name} Soyadi : {bebek.Surname} Dogum gunu : {bebek.Birthday}");
+
+Bebek newBebek = new Bebek("Yavuz","Gecimli");//Creating a baby using the alternative constructor
+
+Console.WriteLine($"Bebegin adi : {newBebek.Name} Soyadi : {newBebek.Surname} Dogum gunu : {newBebek.Birthday}");
+
+Console.ReadKey();
+
